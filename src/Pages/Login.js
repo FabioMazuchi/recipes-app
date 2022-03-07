7cc71817fdfdf2fb986593b1d3ed254fd26bdcd8
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-03-07 19:01:19 -0300

    add req 05

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import NUMBER_6 from '../data';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const checkEmailSenha = () => {
+    const valid = /\S+@\S+\.\S+/;
+    const check = valid.test(email);
+    if (check && senha.length > NUMBER_6) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    checkEmailSenha();
+  }, [email, senha]);
+
   return (
     <form>
-      <input data-testid="email-input" type="text" placeholder="Email..." />
-      <input data-testid="password-input" type="password" placeholder="Senha..." />
-      <button data-testid="login-submit-btn" type="button">Enter</button>
+      <input
+        value={ email }
+        data-testid="email-input"
+        type="text"
+        placeholder="Email..."
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        value={ senha }
+        data-testid="password-input"
+        type="password"
+        placeholder="Senha..."
+        onChange={ ({ target }) => setSenha(target.value) }
+      />
+      <button disabled={ disabled } data-testid="login-submit-btn" type="button">
+        Enter
+      </button>
     </form>
   );
 }
