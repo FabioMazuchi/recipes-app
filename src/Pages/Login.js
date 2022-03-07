6cdd530b0896e1d6a4048140cf61290b75741487
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-03-07 18:10:02 -0300

    add req 02

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>Página de Login</h1>
+    <form>
+      <input data-testid="email-input" type="text" placeholder="Email..." />
+      <input data-testid="password-input" type="password" placeholder="Senha..." />
+      <button data-testid="login-submit-btn" type="button">Enter</button>
+    </form>
   );
 }
 
