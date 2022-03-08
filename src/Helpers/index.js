c8965aeaf22db4ed7d827f1342b7dad2ebbc695d
AdrianFadiga <adrianmfadiga@gmail.com>
2022-03-08 19:01:37 -0300

    ate req 28 ok

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -7,9 +7,9 @@ import { fetchFoodByIngredient,
 } from '../Services';
 import NUMBER_6 from '../data';
 
-export const fetchFoodRecipe = (searchType, searchValue) => {
+export const fetchFoodRecipe = async (searchType, searchValue) => {
   const switchCaseModerno = {
-    ingredient: fetchFoodRecipe,
+    ingredient: fetchFoodByIngredient,
     name: fetchFoodByName,
     firstLetter: fetchFoodByFirstLetter,
   };
@@ -29,20 +29,9 @@ export const fetchDrinkRecipe = (searchType, searchValue) => {
   }
 };
 
-
-// export const redirectToDetails = (data) => {
-//   const history = useHistory();
-//   if (Object.keys(data[0].includes('Meal'))) {
-//     return history.push(`/foods/${data[0].idMeal}`);
-//   } else {
-//     history.push(`/drinks/${data[0].idDrink}`);
-//   }
-// };
-
 export const checkEmailSenha = (email, senha) => {
   const valid = /\S+@\S+\.\S+/;
   const check = valid.test(email);
   if (check && senha.length > NUMBER_6) return true;
   return false;
 };
-
