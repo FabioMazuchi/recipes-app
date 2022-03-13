7bd31bc5bb7c55052e1c81abc942b8f7a85217e4
AdrianFadiga <adrianmfadiga@gmail.com>
2022-03-13 01:15:16 -0300

    refatoradas funcoes e as pages FoodRecipe e DrinkRecipe

--- src/Pages/FoodsInProgress.js
+++ src/Pages/FoodsInProgress.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+// import { useParams, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import MyContext from '../MyContext/MyContext';
 
 function FoodsInProgress() {
+  const { store: { isFavorited,
+    foodRecipe,
+    foodIngredients } } = useContext(MyContext);
+  //   const { id } = useParams();
+  const { pathname } = useLocation();
+  const [showLinkCopied, setShowLinkCopied] = useState(false);
+
   return (
-    <div>
-      <button type="button">Compartilhar</button>
-      <button type="button">Favoritar</button>
-    </div>
+    <>
+      {foodRecipe.map(
+        ({
+          idMeal,
+          strCategory,
+          strMeal,
+          strMealThumb,
+          strInstructions,
+        }) => (
+          <div key={ idMeal }>
+            <button
+              data-testid="share-btn"
+              type="button"
+              value={ `http://localhost:3000${pathname}` }
+              // Source: https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard
+              onClick={ ({ target }) => {
+                navigator.clipboard.writeText(target.value);
+                setShowLinkCopied(true);
+              } }
+            >
+              Compartilhar
+            </button>
+            {showLinkCopied
+            && <p>Link copied!</p>}
+            <input
+              data-testid="favorite-btn"
+              type="image"
+              src={ isFavorited ? blackHeartIcon : whiteHeartIcon }
+              alt="favoriteRecipe"
+              onClick={ () => favoritarReceita() }
+            />
+            <h3 data-testid="recipe-category">{strCategory}</h3>
+            <h2 data-testid="recipe-title">{strMeal}</h2>
+            <img data-testid="recipe-photo" src={ strMealThumb } alt="oi" />
+            <h2>Ingredientes:</h2>
+            {foodIngredients.map(({ ingredient, measure }, i) => (
+              <p
+                data-testid={ `${i}-ingredient-step` }
+                key={ i }
+              >
+                { `${ingredient} - ${measure}` }
+              </p>
+            ))}
+            <h3>Modo de preparo:</h3>
+            <p data-testid="instructions">{strInstructions}</p>
+          </div>
+        ),
+      )}
+    </>
   );
 }
 
--- src/Services/index.js
+++ src/Services/index.js
@@ -8,7 +8,6 @@ export const fetchFoods = async (endpoint, comparison) => {
     global.alert('Your search must have only 1 (one) character');
   }
   const data = await (await fetch(`${URL_FOOD}${endpoint}`)).json();
-  data.meals.slice(0, MAX_LENGTH);
   return data.meals.slice(0, MAX_LENGTH);
 };
 
@@ -18,95 +17,4 @@ export const fetchDrinks = async (endpoint, comparison) => {
   }
   const data = await (await fetch(`${URL_DRINK}${endpoint}`)).json();
   return data.drinks.slice(0, MAX_LENGTH);
-};
-
-export const getIngredients = (array) => {
-  const keys = Object.entries(array[0]);
-  const arrayIngred = keys
-    .filter((k) => k[0].includes('Ingredient') && k[1] !== '' && k[1] !== null);
-  const arrayMeasure = keys
-    .filter((k) => k[0].includes('strMeasure') && k[1] !== '' && k[1] !== null);
-  const ingred = arrayIngred.map((ingr) => ingr[1]);
-  const measure = arrayMeasure.map((measur) => measur[1]);
-  const finalResult = ingred.reduce(((acc, ingr, i) => {
-    const obj = { ingredient: '', measure: '' };
-    obj.ingredient = ingr;
-    obj.measure = measure[i];
-    acc.push(obj);
-    return acc;
-  }), []);
-  return finalResult;
-};
-
-const returnObjFood = (obj) => {
-  const { idMeal, strArea, strCategory, strMeal, strMealThumb } = obj[0];
-  const object = {
-    id: idMeal,
-    type: 'food',
-    nationality: strArea,
-    category: strCategory,
-    alcoholicOrNot: '',
-    name: strMeal,
-    image: strMealThumb };
-  return object;
-};
-
-export const removeFavStorageFood = (id) => {
-  console.log(id);
-  const res = localStorage.getItem('favoriteRecipes');
-  if (res !== null) {
-    const valor = JSON.parse(res);
-    const filtro = valor.filter(({ id: idMeal }) => idMeal !== id);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(filtro));
-  }
-};
-
-export const removeFavStorageDrink = (id) => {
-  const res = localStorage.getItem('favoriteRecipes');
-  if (res !== null) {
-    const valor = JSON.parse(res);
-    const filtro = valor.filter(({ id: idDrink }) => idDrink !== id);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(filtro));
-  }
-};
-
-export const saveFoodFavStorage = (obj) => {
-  const fav = returnObjFood(obj);
-  const array = [];
-  const res = localStorage.getItem('favoriteRecipes');
-  if (res === null) {
-    array.push(fav);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(array));
-  } else {
-    const valor = JSON.parse(res);
-    valor.push(fav);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(valor));
-  }
-};
-
-const returnObjDrink = (obj) => {
-  const { idDrink, strCategory, strDrink, strAlcoholic, strDrinkThumb } = obj[0];
-  const object = {
-    id: idDrink,
-    type: 'drink',
-    nationality: '',
-    category: strCategory,
-    alcoholicOrNot: strAlcoholic,
-    name: strDrink,
-    image: strDrinkThumb };
-  return object;
-};
-
-export const saveDrinkFavStorage = (obj) => {
-  const fav = returnObjDrink(obj);
-  const array = [];
-  const res = localStorage.getItem('favoriteRecipes');
-  if (res === null) {
-    array.push(fav);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(array));
-  } else {
-    const valor = JSON.parse(res);
-    valor.push(fav);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(valor));
-  }
 };
