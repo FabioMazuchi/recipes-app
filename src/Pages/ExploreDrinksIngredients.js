8c54822a4c372bebab606a14b3f31781db7a7b4b
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-03-14 19:03:18 -0300

    add req 76

--- src/Pages/ExploreDrinksIngredients.js
+++ src/Pages/ExploreDrinksIngredients.js
@@ -1,20 +1,40 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import MyContext from '../MyContext/MyContext';
+import { fetchDrinks } from '../Services';
 
 function ExploreDrinksIngredients() {
   const { store: { setPageTitle, setShowSearchIcon } } = useContext(MyContext);
+  const [ingredientes, setIngredientes] = useState([]);
+
+  const requestIngredients = async () => {
+    const res = await fetchDrinks('list.php?i=list', 1);
+    setIngredientes(res);
+  };
 
   useEffect(() => {
     setPageTitle('Explore Ingredients');
     setShowSearchIcon(false);
   }, []);
 
+  useEffect(() => {
+    requestIngredients();
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>Página de ExploreDrinksIngredients</h1>
+      {ingredientes.map((({ strIngredient1 }, i) => (
+        <div key={ i } data-testid={ `${i}-ingredient-card` }>
+          <img
+            data-testid={ `${i}-card-img` }
+            alt={ strIngredient1 }
+            src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png` }
+          />
+          <p data-testid={ `${i}-card-name` }>{strIngredient1}</p>
+        </div>
+      )))}
       <Footer />
     </>
   );
--- src/Pages/ExploreFoodsIngredients.js
+++ src/Pages/ExploreFoodsIngredients.js
@@ -1,20 +1,42 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import MyContext from '../MyContext/MyContext';
+import { fetchFoods } from '../Services';
 
 function ExploreFoodsIngredients() {
-  const { store: { setPageTitle, setShowSearchIcon } } = useContext(MyContext);
+  const {
+    store: { setPageTitle, setShowSearchIcon },
+  } = useContext(MyContext);
+  const [ingredientes, setIngredientes] = useState([]);
+
+  const requestIngredients = async () => {
+    const res = await fetchFoods('list.php?i=list', 1);
+    setIngredientes(res);
+  };
 
   useEffect(() => {
     setPageTitle('Explore Ingredients');
     setShowSearchIcon(false);
   }, []);
 
+  useEffect(() => {
+    requestIngredients();
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>Página de ExploreFoodsIngredients</h1>
+      {ingredientes.map(({ strIngredient }, i) => (
+        <div key={ i } data-testid={ `${i}-ingredient-card` }>
+          <img
+            data-testid={ `${i}-card-img` }
+            alt={ strIngredient }
+            src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
+          />
+          <p data-testid={ `${i}-card-name` }>{strIngredient}</p>
+        </div>
+      ))}
       <Footer />
     </>
   );
